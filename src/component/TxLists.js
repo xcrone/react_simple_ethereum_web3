3455ac062f64cde923f32baadb89f16adecb838f
zahir <48623158+xcrone@users.noreply.github.com>
2021-06-25 22:27:01 +0800

    try fixing txlist account undefined

--- src/component/TxLists.js
+++ src/component/TxLists.js
@@ -3,18 +3,19 @@ import Wallets from "../Wallets"
 
 const TxLists = () => {
     const {data} = useContext(Wallets.WalletsContext);
+    let account = data.account;
     let [txs, setTxs] = useState([]);
 
     useEffect( () => {
         const getData = async () => {
-            const res = await Wallets.TxListing(data.account, {
+            const res = await Wallets.TxListing(account, {
                 page: 1,
                 offset: 20
             }); 
             setTxs(res);
         }
         getData();
-    }, [data.account]);
+    }, [account]);
 
     return (
         <div className="card">
@@ -55,11 +56,11 @@ const TxLists = () => {
                                     <td>{Wallets.EpochToTimeAgo(tx.timeStamp)}</td>
                                     <td>
                                         <a target="_blank" href={"https://testnet.bscscan.com/address/"+tx.from}>
-                                            {tx.from === data.account ? "My Account" : Wallets.ShortAddress(tx.from)}
+                                            {tx.from === account ? "My Account" : Wallets.ShortAddress(tx.from)}
                                         </a>
                                     </td>
                                     <td>
-                                        {tx.from === data.account ? "OUT" : "IN" }
+                                        {tx.from === account ? "OUT" : "IN" }
                                     </td>
                                     <td>
                                         {tx.to === "" ? (
@@ -68,7 +69,7 @@ const TxLists = () => {
                                             </a>
                                         ) : (
                                             <a target="_blank" href={"https://testnet.bscscan.com/address/"+tx.to}>
-                                                {tx.to === data.account ? "My Account" : Wallets.ShortAddress(tx.to)}
+                                                {tx.to === account ? "My Account" : Wallets.ShortAddress(tx.to)}
                                             </a>
                                         )}
                                     </td>
